d2dcf427b63cf8337617aab34e8bd62a1ed111a2
xuwei-k <6b656e6a69@gmail.com>
2015-05-30 15:02:58 +0900

    wip

--- core/src/main/scala/scalaprops/Fun.scala
+++ core/src/main/scala/scalaprops/Fun.scala
@@ -16,4 +16,11 @@ object Fun {
       Gen[Func[A, B]], B
     )(_.mkFun(_))
 
+
+  implicit def shrink[A, B](implicit B: Shrink[B]): Shrink[Fun[A, B]] =
+    Shrink.shrink[Fun[A, B]]{ case Fun(p1, d1, _) =>
+      Shrink[((Func[A, B]), B)].f((p1, d1)).map{
+        case (p2, d2) => p2.mkFun(d2)
+      }
+    }
 }
